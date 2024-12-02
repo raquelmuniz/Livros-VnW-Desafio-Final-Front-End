9e0750cc2847579686ae6a3a4af3c86077680064
Raquel Muniz <munizraquel70@gmail.com>
2024-12-02 19:47:37 -0300

    ajustando importacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/header/header"
-import Footer from './components/Footer/footer'
+import Footer from './components/footer/Footer'
 import './global.scss'
 
 export default function App(){
