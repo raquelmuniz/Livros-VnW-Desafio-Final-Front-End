b273dcb58dbec5211ab31b79a5566782474fe19b
Raquel Muniz <munizraquel70@gmail.com>
2024-12-02 19:30:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header/header"
+import Footer from './components/Footer/footer'
+import './global.scss'
+
+export default function App(){
+  return(
+    <>
+    <Header/> 
+    <Footer/> 
+    </>
+  )
+}
---
+++ src/pages/doados/Doados.jsx
@@ -0,0 +1,7 @@
+export default function Doados(){
+    return(
+        <section>
+            <h2>Eu sou a Livros Doados</h2>
+        </section>
+    )
+}
